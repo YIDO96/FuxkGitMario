4096cef9b11688150822e6867e713e989c6a7714
SongKyun <89376275+SongKyun@users.noreply.github.com>
2024-09-03 03:06:52 +0900

    UI ing
    Start, Life ing

---
+++ Source/FuxkGitMario/UI/LifeWidget.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "LifeWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FUXKGITMARIO_API ULifeWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+	/*UPROPERTY(EditAnywhere, meta = (BindWidget))
+	class UTextBlock* scoreText;*/
+	
+};
---
+++ Source/FuxkGitMario/UI/StartWidget.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "StartWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FUXKGITMARIO_API UStartWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	/*UPROPERTY(EditAnywhere, meta = (BindWidget))
+	class UTextBlock* scoreText;
+
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	class UTexBlock* scoreData;*/
+};
